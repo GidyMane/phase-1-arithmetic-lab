e0c9063197816c0cd764a6a286e3f2cece2b8ae4
GidyMane <muragegideon2000@gmail.com>
2023-09-20 08:16:20 -0700

    Complete Assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2 ;
+
+ let random = Math.floor(Math.random() * 4 + 1 );
+
+ let num3 = 32 ;
+ let num4 = 7;
+ let mod = num3 % num4;
+
+ let max = Math.max(4, 7, 16, 20, 10, 18);
+ 
+
+
